7e17a010a11dd1a11785e10991e3ace11abe2b2c
yymin1022 <yymin1022@gmail.com>
2024-03-01 17:10:55 +0900

    Update Project : Fixed Indentations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideMenu from "@/app/_component/SideMenu/SideMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dev. LR Blog",
-  description: "1인개발자 LR의 IT블로그",
+    title: "Dev. LR Blog",
+    description: "1인개발자 LR의 IT블로그",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <SideMenu/>
+                <div>
+                    {children}
+                </div>
+            </body>
+        </html>
+    );
 }
