6c13ce47d8cd4b0b9bb44231c80ab9241bf50ff2
yymin1022 <yymin1022@gmail.com>
2024-03-02 15:34:37 +0900

    Update Tailwind Config : Defined Color Presets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,17 @@ const config: Config = {
       ],
       theme: {
             extend: {
-                  
+                  colors: {
+                        primary: {
+                              blog_black: "#000000",
+                              blog_blue: "#164EAB",
+                              blog_gray: "#606060",
+                              blog_lightgray_1: "#808080",
+                              blog_lightgray_2: "#AAAAAA",
+                              blog_lightgray_3: "#DDDDDD",
+                              blog_white: "#FFFFFF"
+                        }
+                  }
             },
       },
       plugins: [],
