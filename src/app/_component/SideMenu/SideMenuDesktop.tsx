230444b302d977a854209cec8890cb83f5ae48d3
yymin1022 <yymin1022@gmail.com>
2024-03-02 18:34:15 +0900

    Update SideMenuDesktop : Added Basic Elements

--- src/app/_component/SideMenu/SideMenuDesktop.tsx
+++ src/app/_component/SideMenu/SideMenuDesktop.tsx
@@ -1,13 +1,35 @@
 const SideMenuDesktop = () => {
     return(
-        <div className="h-full w-[400px] flex flex-col bg-primary-blog_blue">
-            Side Menu
+        <div className="h-full w-[400px] flex flex-col justify-end bg-primary-blog_blue">
+            <SideMenuTitle />
+            <SideMenuDivider />
+            <SideMenuNav />
+        </div>
+    );
+}
+
+const SideMenuDivider = () => {
+    return(
+        <div>
+            Divider
+        </div>
+    );
+}
+
+const SideMenuNav = () => {
+    return(
+        <div>
+            Nav
         </div>
     );
 }
 
 const SideMenuTitle = () => {
-
+    return(
+        <div>
+            Title
+        </div>
+    );
 }
 
 export default SideMenuDesktop;
