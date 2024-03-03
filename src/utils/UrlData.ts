e4570895a8b4add72ac7de0b0f5bf6878677924c
yymin1022 <yymin1022@gmail.com>
2024-03-04 03:25:18 +0900

    Update UrlData : Addded Enum Datas

---
+++ src/utils/UrlData.ts
@@ -0,0 +1,7 @@
+enum UrlData{
+    facebook = "https://facebook.com",
+    github = "https://github.com/yymin1022",
+    googleplay = "https://play.google.com/store/apps/dev?id=5289403177081881132",
+    instagram = "https://instagram.com/useful_min",
+    linkedin = "https://www.linkedin.com/in/yymin1022"
+}
