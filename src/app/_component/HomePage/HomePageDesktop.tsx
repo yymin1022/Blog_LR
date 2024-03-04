1c456bb6766e597f30da8b867277333ef297d67a
yymin1022 <yymin1022@gmail.com>
2024-03-05 01:57:57 +0900

    Update HomePageDesktop : Added Nav Buttons

--- src/app/_component/HomePage/HomePageDesktop.tsx
+++ src/app/_component/HomePage/HomePageDesktop.tsx
@@ -1,10 +1,27 @@
+import Link from "next/link";
+
 const HomePageDesktop = () => {
     return(
         <div className="flex flex-col mt-[200px] mx-[100px]">
             <HomePageDesktopTitle isBlue={false} text={"안녕하세요"} />
             <HomePageDesktopTitle isBlue={true} text={"대학생 1인개발자"} />
             <HomePageDesktopTitle isBlue={false} text={"LR입니다."} />
+            <div className="h-[50px]" />
+            <HomePageDesktopNav path={"/blog"} text={"Blog"} />
+            <HomePageDesktopNav path={"/project"} text={"Project"} />
+            <HomePageDesktopNav path={"/solving"} text={"Problem Solving"} />
+            <HomePageDesktopNav path={"/about"} text={"About Me"} />
         </div>
+    );
+}
+
+const HomePageDesktopNav = ({path, text}: {path: string, text: string}) => {
+    return(
+        <Link href={path}>
+            <div className="ms-[10px] my-[5px] font-nanum-r text-xl text-primary-blog_black">
+                {text}
+            </div>
+        </Link>
     );
 }
 
