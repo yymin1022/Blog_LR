87b98d8b1bf93469612d3b332d17c17a5da2fbf7
yymin1022 <yymin1022@gmail.com>
2026-05-24 03:03:52 +0900

    Fix Utterances Component : Added cleanup to prevent error during unmount

--- src/app/[type]/_component/Utterances/Utterances.tsx
+++ src/app/[type]/_component/Utterances/Utterances.tsx
@@ -6,17 +6,23 @@ const Utterances = () => {
     const utterancesRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
+        const utterances = document.createElement("script");
+        utterances.setAttribute("src", "https://utteranc.es/client.js");
+        utterances.setAttribute("async", "true");
+        utterances.setAttribute("crossorigin", "anonymous");
+        utterances.setAttribute("repo", "yymin1022/Blog_LR_Comments");
+        utterances.setAttribute("theme", "github-light");
+        utterances.setAttribute("issue-term", "pathname");
+
         if (utterancesRef.current) {
-            utterancesRef.current.innerHTML = "";
-            const utterances = document.createElement("script");
-            utterances.setAttribute("src", "https://utteranc.es/client.js");
-            utterances.setAttribute("async", "true");
-            utterances.setAttribute("crossorigin", "anonymous");
-            utterances.setAttribute("repo", "yymin1022/Blog_LR_Comments");
-            utterances.setAttribute("theme", "github-light");
-            utterances.setAttribute("issue-term", "pathname");
             utterancesRef.current.appendChild(utterances);
         }
+
+        return () => {
+            if (utterancesRef.current) {
+                utterancesRef.current.innerHTML = "";
+            }
+        };
     }, []);
 
     return <div ref={utterancesRef} className="w-full"></div>;
